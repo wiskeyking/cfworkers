49e23eff04abf24cf527feaec9957f617b22e6ea
wiskeyking <jin.orlane@gmail.com>
2024-01-24 21:08:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,12 +11,8 @@ let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
-// v2board api environment variables
-let nodeId = ''; // 1
+// v2board api environment variables (optional) deprecated, please use planetscale.com instead
 
-let apiToken = ''; //abcdefghijklmnopqrstuvwxyz123456
-
-let apiHost = ''; // api.v2board.com
 
 if (!isValidUUID(userID)) {
     throw new Error('uuid is not valid');
